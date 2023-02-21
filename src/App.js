d24c0b4ddf48e8a9c8909276ce414f40facc424f
graysonzhang <graysonzhang@126.com>
2023-02-21 22:54:41 +0800

    add login button on home page

--- src/App.js
+++ src/App.js
@@ -1,67 +1,91 @@
 import './App.css';
-import HomeHeader from './components/HomeHeader';
-import { Breadcrumb, Layout, theme } from 'antd';
+import { Breadcrumb, Layout, theme, Button, Space, Input, Image } from 'antd';
 import { Outlet } from 'react-router-dom';
 
-const { Content, Footer } = Layout;
+const { Header, Content, Footer } = Layout;
+const { Search } = Input;
 
 function App() {
+	const {
+		token: { colorBgContainer }
+	} = theme.useToken();
 
-  const {
-    token: { colorBgContainer },
-  } = theme.useToken();
+	const onSearch = (value) => console.log(value);
 
-  return (
-    <Layout>
-      <HomeHeader />
-      <Content
-        className="site-layout"
-        style={{
-          padding: '0 50px',
-        }}
-      >
-        <Breadcrumb
-          style={{
-            margin: '16px 0',
-          }}
-        >
-          <Breadcrumb.Item>Home</Breadcrumb.Item>
-          <Breadcrumb.Item>List</Breadcrumb.Item>
-          <Breadcrumb.Item>App</Breadcrumb.Item>
-        </Breadcrumb>
-        <div className='content'
-          style={{
-            padding: 24,
-            minHeight: 680,
-            background: colorBgContainer,
-          }}
-        >
-          <div className='main'>
-            <Outlet />
-          </div>
-          <div className='slide'>
+	return (
+		<Layout>
+			<Header
+				style={{
+					position: 'sticky',
+					top: 0,
+					zIndex: 1,
+					background: 'rgba(0, 0, 0)',
+					height: 80,
+					width: '100%'
+				}}
+			>
+				{/* <div
+					style={{
+						width: 1000
+					}}
+				></div> */}
+				<div
+					style={{
+						float: 'left',
+						width: 800,
+						height: 31,
+						margin: '16px 24px 16px 0',
+						background: 'rgba(255, 255, 255, 0.2)'
+					}}
+				></div>
 
-          </div>
-        </div>
-        {/* <div
-          style={{
-            padding: 24,
-            minHeight: 380,
-            background: colorBgContainer,
-          }}
-        >
-          Content
-        </div> */}
-      </Content>
-      <Footer
-        style={{
-          textAlign: 'center',
-        }}
-      >
-        Ant Design ©2023 Created by Ant UED
-      </Footer>
-    </Layout>
-  );
+				<Space
+					style={{
+						float: 'right'
+					}}
+				>
+					<Button type="primary">登录</Button>
+					<Button>注册</Button>
+				</Space>
+			</Header>
+			<Content
+				className="site-layout"
+				style={{
+					padding: '0 50px'
+				}}
+			>
+				<Breadcrumb
+					style={{
+						margin: '16px 0'
+					}}
+				>
+					<Breadcrumb.Item>Home</Breadcrumb.Item>
+					<Breadcrumb.Item>List</Breadcrumb.Item>
+					<Breadcrumb.Item>App</Breadcrumb.Item>
+				</Breadcrumb>
+				<div
+					className="content"
+					style={{
+						padding: 24,
+						minHeight: 1680,
+						background: colorBgContainer
+					}}
+				>
+					<div className="main">
+						<Outlet />
+					</div>
+					<div className="slide"></div>
+				</div>
+			</Content>
+			<Footer
+				style={{
+					textAlign: 'center'
+				}}
+			>
+				Ant Design ©2023 Created by Ant UED
+			</Footer>
+		</Layout>
+	);
 }
 
 export default App;
