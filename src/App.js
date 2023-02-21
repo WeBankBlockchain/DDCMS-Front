fa2b92647a4c58984489073c439508d1725d5756
graysonzhang <graysonzhang@126.com>
2023-02-21 15:27:22 +0800

    add home page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,105 @@
-import logo from './logo.svg';
 import './App.css';
+import HomeHeader from './components/HomeHeader';
+import { Breadcrumb, Layout, Menu, theme } from 'antd';
+import { Outlet } from 'react-router-dom';
+
+const { Header, Content, Footer } = Layout;
 
 function App() {
+
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    // <div className="App">
+    //   <HomeHeader />
+    //   <Content className='main'>
+    //     <div className='content'>
+    //       <Outlet />
+    //     </div>
+    //     <div className='tags'>
+    //       show tags
+    //     </div>
+    //   </Content>
+    // </div>
+    <Layout>
+      <Header
+        style={{
+          position: 'sticky',
+          top: 0,
+          zIndex: 1,
+          width: '100%',
+        }}
+      >
+        <div
+          style={{
+            float: 'left',
+            width: 120,
+            height: 31,
+            margin: '16px 24px 16px 0',
+            background: 'rgba(255, 255, 255, 0.2)',
+          }}
+        />
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={['2']}
+          items={new Array(3).fill(null).map((_, index) => ({
+            key: String(index + 1),
+            label: `nav ${index + 1}`,
+          }))}
+        />
+      </Header>
+      <Content
+        className="site-layout"
+        style={{
+          padding: '0 50px',
+        }}
+      >
+        <Breadcrumb
+          style={{
+            margin: '16px 0',
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>List</Breadcrumb.Item>
+          <Breadcrumb.Item>App</Breadcrumb.Item>
+        </Breadcrumb>
+        <div className='content'
+          style={{
+            padding: 24,
+            minHeight: 680,
+            background: colorBgContainer,
+          }}
+        >
+          <div className='main'
+            
+          >
+
+          </div>
+          <div className='slide'>
+
+          </div>
+        </div>
+        {/* <div
+          style={{
+            padding: 24,
+            minHeight: 380,
+            background: colorBgContainer,
+          }}
+        >
+          Content
+        </div> */}
+      </Content>
+      <Footer
+        style={{
+          textAlign: 'center',
+        }}
+      >
+        Ant Design ©2023 Created by Ant UED
+      </Footer>
+    </Layout>
   );
 }
 
