2b3e9f0c6476840704e7c22e7cb93350ce794ebd
graysonzhang <graysonzhang@126.com>
2023-03-01 00:30:06 +0800

    add eslint ignore useEffect warning

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -31,6 +31,7 @@ export default function Home() {
         message.error(res.msg);
       }
     })
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const onLoadMore = () => {
