e1c888819015736e343ca1f23464e3ff4e620a28
dalaocu <njumjy06@126.com>
2023-03-06 20:16:17 +0800

    upload file

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,11 +1,23 @@
 import { UploadOutlined } from '@ant-design/icons';
 import { Button, message, Upload } from 'antd';
+import {baseURL} from "../request/service.js";
+
+const uploadFile = (file) => {
+  // create a FormData object
+  const formData = new FormData();
+  // append the file to the formData
+  formData.append('file', file);
+  // append other data to the formData
+  formData.append('name', 'test');
+  return formData;
+};
+
+const url = baseURL + 'file/upload';
+
 const props = {
   name: 'file',
-  action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
-  headers: {
-    authorization: 'authorization-text',
-  },
+  action: url,
+  data: {uploadFile},
   onChange(info) {
     if (info.file.status !== 'uploading') {
       console.log(info.file, info.fileList);
