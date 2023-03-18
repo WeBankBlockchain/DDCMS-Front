19449c01f3cd8f38883afa6a005d8c9e56443003
graysonzhang <graysonzhang@126.com>
2023-03-18 10:34:05 +0800

    add token header

--- src/request/service.js
+++ src/request/service.js
@@ -1,31 +1,35 @@
-import axios from "axios";
+import axios from 'axios';
 
-export const baseURL = "http://116.205.167.248:10880/api/";
+export const baseURL = 'http://116.205.167.248:10880/api/';
 
 const axiosOption = {
-  baseURL: baseURL,
-  // baseURL: 'http://127.0.0.1:10880/api/',
-  timeout: 5000,
+	baseURL: baseURL,
+	// baseURL: 'http://127.0.0.1:10880/api/',
+	timeout: 5000
 };
 
 const instance = axios.create(axiosOption);
 
 instance.interceptors.request.use(
-  (config) => {
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
+	(config) => {
+		let token = window.localStorage.getItem('token');
+		if (token) {
+			config.headers['Authorization'] = token;
+		}
+		return config;
+	},
+	(error) => {
+		return Promise.reject(error);
+	}
 );
 
 instance.interceptors.response.use(
-  (response) => {
-    return response.data;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
+	(response) => {
+		return response.data;
+	},
+	(error) => {
+		return Promise.reject(error);
+	}
 );
 
 export default instance;
