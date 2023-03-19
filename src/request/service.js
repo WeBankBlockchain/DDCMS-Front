60b3dd723da021f4f0c05235b33e2cc6442e63cf
dalaocu <njumjy06@126.com>
2023-03-19 21:45:01 +0800

    service.js

--- src/request/service.js
+++ src/request/service.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { message } from "antd";
 
 export const baseURL = 'http://116.205.167.248:10880/api/';
 
@@ -11,25 +12,35 @@ const axiosOption = {
 const instance = axios.create(axiosOption);
 
 instance.interceptors.request.use(
-	(config) => {
-		let token = window.localStorage.getItem('token');
-		if (token) {
-			config.headers['Authorization'] = token;
-		}
-		return config;
-	},
-	(error) => {
-		return Promise.reject(error);
-	}
+  (config) => {
+    // 获取 JWT 令牌
+    const token = localStorage.getItem("token");
+    // 如果存在令牌，则在请求头中添加 Authorization 字段
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
 );
 
 instance.interceptors.response.use(
-	(response) => {
-		return response.data;
-	},
-	(error) => {
-		return Promise.reject(error);
-	}
+  (response) => {
+    return response.data;
+  },
+  (error) => {
+    // 处理 HTTP 错误
+    if (error.response) {
+      message.error(
+        `请求失败: ${error.response.status} ${error.response.statusText}`
+      );
+    } else {
+      message.error(error.message);
+    }
+    return Promise.reject(error);
+  }
 );
 
 export default instance;
