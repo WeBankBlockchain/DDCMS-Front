4543622a195a0296526497d64b7917e1c043acad
arc0035 <aoronchu@gmail.com>
2023-03-21 11:43:46 +0800

    菜单从后端读取

--- src/components/AdminSlider.jsx
+++ src/components/AdminSlider.jsx
@@ -5,7 +5,7 @@ import {
   UserOutlined,
 } from "@ant-design/icons";
 import { Layout, Menu ,message } from "antd";
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { GetMenuByRoleApi } from "../request/api";
 
 const { Sider } = Layout;
@@ -18,17 +18,18 @@ const MenuItemGroup = Menu.ItemGroup;
 export default function AdminSlider(props) {
 
   const [menus, setMenus] = useState([]);
+  const navigate = useNavigate();
 
-
-  useEffect(
+  useEffect(()=>{
     GetMenuByRoleApi({}).then(res=>{
       if (res.code === 0){
+        // console.log(res.data);
         setMenus(res.data);
       } else{
         message.error(res.message);
       }
     })  
-    ,[]
+  },[]
   )
 
   return (
@@ -43,9 +44,30 @@ export default function AdminSlider(props) {
         style={{
           height: "100%",
         }}
+        key='menu'
       >
-
+        {menuDataToMenu(navigate, menus)}
       </Menu>
     </Sider>
   );
+}
+
+function menuDataToMenu(navigate, menuDatas) {
+  const menus = [];
+  for (const m of menuDatas){
+    if (m.children==null || m.children.length == 0){
+      const menuItem = (<Menu.Item key={m.menuId} onClick={()=>(navigate(m.menuUrl))}>{m.menuName}</Menu.Item>)
+      menus.push(menuItem);
+    } else{
+      const items = menuDataToMenu(navigate, m.children);
+      const subMenu = 
+      (<SubMenu key={m.menuId} title={m.menuName}>
+        {
+        items} 
+      </SubMenu>)
+
+      menus.push(subMenu);
+    }
+  }
+  return menus
 }
