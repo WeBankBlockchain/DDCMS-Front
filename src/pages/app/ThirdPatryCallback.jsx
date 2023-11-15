5032fd7613e806faee474da916b1eeb4374bdc7f
xun chen <ashinnotfound@qq.com>
2023-11-15 11:12:38 +0800

    Task5 实现DDCMS的第三方登录功能(前端) (#1)
    * feat(.env):新增github应用clientId配置项
    
    * feat(router):新增thirdPatryCallback路由配置
    
    * feat(request):新增绑定第三方和使用第三方登录的api
    
    * feat(app):新增对第三方回调的处理
    
    * feat(app):新增使用第三方登录的功能
    
    * feat(admin):新增绑定第三方账号的功能
    
    * improvement(app): 优化错误提示信息

---
+++ src/pages/app/ThirdPatryCallback.jsx
@@ -0,0 +1,51 @@
+import { message } from 'antd';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { BindThirdPartyApi, LoginWithThirdPartyApi } from '../../request/api';
+import HomeHeader from "../../components/header/HomeHeader";
+
+export default function ThirdPatryCallback() {
+  const navigate = useNavigate();
+
+  const type = new URLSearchParams(window.location.search).get('type');
+  const code = new URLSearchParams(window.location.search).get('code');
+  const operate = localStorage.getItem("thirdPartyOperation")
+  const request = {
+    type: type,
+    code: code
+  }
+  if (operate === "bind") {
+    //绑定第三方账号
+    BindThirdPartyApi(request).then(res => {
+      if (res.code === 0) {
+        message.success("绑定成功");
+        setTimeout(() => navigate("/admin/user/info"), 1000);
+      }else {
+        message.error(res.debugMsg);
+        setTimeout(() => navigate("/admin/user/info"), 1000);
+      }
+    });
+  } else {
+    //使用第三方登录
+    LoginWithThirdPartyApi(request).then(res => {
+      if (res.code === 0) {
+        message.success("登录成功!");
+        localStorage.setItem("userName", res.data.userName);
+        localStorage.setItem("token", res.data.token);
+        localStorage.setItem("accountType", res.data.accountType);
+        setTimeout(() => navigate("/admin"), 1000);
+      } else {
+        setTimeout(() => navigate("/login"), 1000);
+        message.error(res.debugMsg);
+      }
+    });
+  }
+
+
+  return (
+    <div>
+      <HomeHeader ></HomeHeader>
+      <div style={{ left: "50%", top: "50%" }}><h1>正在使用第三方账号登录...</h1></div>
+    </div>
+  );
+}
